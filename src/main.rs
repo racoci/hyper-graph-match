af33fe03c2f76f6a816c1fb7b7ff93149b48e74a
Racoci <racoci.0@gmail.com>
2023-03-09 20:03:07 -0300

    Build a Hypergraph from each line read.
    Each line must be interpreted as an edge.
    
    We define a new struct Hypergraph to represent our hypergraph. It contains two fields: a HashMap of vertices, where each vertex name maps to a set of line numbers where that vertex appears, and a Vec of edges, where each edge is a set of vertex names.
    
    We define methods on the Hypergraph struct to add vertices and edges.
    
    We create a new Hypergraph instance, a line number counter, and a handle to standard input using io::stdin().
    
    We loop over each line of input using stdin.lock().lines().
    
    For each line, we check if it's equal to "-", which is our termination character. If it is, we break out of the loop.
    
    Otherwise, we create a new HashSet to store the vertices for this line's edge, and loop over each word in the line using split_whitespace().
    
    For each word, we add it as a vertex to the Hypergraph, and insert it into our vertices set.
    
    After processing all the words in the line, we add the vertices set as a new edge to the Hypergraph.
    
    We increment the line_num counter to keep track of which line we're on.
    
    Finally, we print out the contents of the Hypergraph using println!("{:#?}", hypergraph).
    
    The resulting Hypergraph contains information about which vertices appear on each line, and which vertices are connected by each edge.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,35 @@
 use std::io::{self, BufRead};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+
+#[derive(Debug)]
+struct Hypergraph {
+    vertices: HashMap<String, HashSet<usize>>,
+    edges: Vec<HashSet<String>>,
+}
+
+impl Hypergraph {
+    fn new() -> Self {
+        Self {
+            vertices: HashMap::new(),
+            edges: Vec::new(),
+        }
+    }
+
+    fn add_vertex(&mut self, name: String, line_num: usize) {
+        self.vertices
+            .entry(name)
+            .or_insert_with(HashSet::new)
+            .insert(line_num);
+    }
+
+    fn add_edge(&mut self, vertices: HashSet<String>) {
+        self.edges.push(vertices);
+    }
+}
 
 fn main() {
-    let mut map = HashMap::new();
-    let mut count = 0;
+    let mut hypergraph = Hypergraph::new();
+    let mut line_num = 0;
     let stdin = io::stdin();
 
     for line in stdin.lock().lines() {
@@ -11,15 +37,14 @@ fn main() {
         if line == "-" {
             break;
         }
+        let mut vertices = HashSet::new();
         for word in line.split_whitespace() {
-            map.entry(word.to_owned())
-                .or_insert_with(|| {
-                    let id = count;
-                    count += 1;
-                    id
-                });
+            hypergraph.add_vertex(word.to_owned(), line_num);
+            vertices.insert(word.to_owned());
         }
+        hypergraph.add_edge(vertices);
+        line_num += 1;
     }
 
-    println!("{:#?}", map);
+    println!("{:#?}", hypergraph);
 }
