8aef225903cb2a023ab7067d181a0825964f84da
Lucas Racoci <racoci.0@gmail.com>
2023-04-01 15:36:06 -0300

    Add input instructions and ignore invalid edges
    Previously, the program did not provide clear input instructions to the user, and the `read_hypergraph` function did not check for hyperedges with less than 2 vertices. This commit adds more detailed and intuitive input instructions for the user, and modifies the `read_hypergraph` function to ignore hyperedges with less than 2 vertices. This ensures that only valid hyperedges are added to the hypergraph. The updated code also includes minor formatting changes for readability.

--- src/main.rs
+++ src/main.rs
@@ -41,16 +41,24 @@ fn read_hypergraph<R: BufRead>(reader: R) -> Hypergraph {
             hypergraph.add_vertex(word.to_owned(), line_num);
             vertices.insert(word.to_owned());
         }
-        hypergraph.add_edge(vertices);
-        line_num += 1;
+        if vertices.len() >= 2 {
+            hypergraph.add_edge(vertices);
+            line_num += 1;
+        }
     }
 
     hypergraph
 }
 
 fn main() {
+    println!("\n\nPlease input the first hypergraph, one hyperedge per line,\n\
+     with the vertices of each hyperedge separated by whitespace. \n\
+     Indicate the end of the hypergraph by typing a single dash (-) on\n\
+      a line by itself.\n\n");
     let stdin = io::stdin();
     let hg1 = read_hypergraph(stdin.lock());
+
+    println!("Please input the second hypergraph, using the same format as the first hypergraph.");
     let hg2 = read_hypergraph(stdin.lock());
 
     println!("Hypergraph 1: {:#?}", hg1);
